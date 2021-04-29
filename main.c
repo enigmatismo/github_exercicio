988bb8e773621a4b484e0924818ed8f7df26454c
en?gma <83255492+enigmatismo@users.noreply.github.com>
2021-04-28 23:00:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,18 +11,53 @@ int main(int argc, char *argv[]) {
 
 int calcular_retangulo_area(){
 	float base,altura;
-	printf("Insira a base do retangulo.......:");
+	printf("Insira a base do retangulo.......: ");
 	scanf("%f",&base);
-	printf("Insira a altura do retangulo.....:");
+	printf("Insira a altura do retangulo.....: ");
 	scanf("%f",&altura);
 	printf("A area do retangulo eh: %f ",base*altura);	
 }
 
 int calcular_retangulo_perimetro(){
 	float base,altura;
-	printf("Insira a base do retangulo.......:");
+	printf("Insira a base do retangulo.......: ");
 	scanf("%f",&base);
-	printf("Insira a altura do retangulo.....:");
+	printf("Insira a altura do retangulo.....: ");
 	scanf("%f",&altura);
-	printf("A area do retangulo eh: %f ",2*(base+altura));
+	printf("O perimetro do retangulo eh: %f ",2*(base+altura));
 }
+
+int calcular_circulo_area(){
+	float r, pi;
+	int opc;
+	pi = 3,14;
+	printf("PI foi pré determinado como >> 3,14 <<");
+	printf("Para alterar PI, digite 9; Caso contrário, apenas siga.");
+		switch (opc){
+			case 9:
+				printf("Qual o valor desejado para PI?");
+				scanf("%f", &pi);
+			break;
+		}
+	printf("Insira a razao do circulo.......: ");
+	scanf("%f",&r);
+	printf("A area do circulo eh: %f ", pi * (r*r);
+}
+
+int calcular_circulo_perimetro(){
+	float r, pi;
+	int opc;
+	pi = 3,14;
+	printf("PI foi pré determinado como >> 3,14 <<");
+	printf("Para alterar PI, digite 9; Caso contrário, apenas siga.");
+		switch (opc){
+			case 9:
+				printf("Qual o valor desejado para PI?");
+				scanf("%f", &pi);
+			break;
+		}
+	printf("Insira a razao do circulo.......: ");
+	scanf("%f",&r);
+	printf("O perimetro do circulo eh: %f ", (pi * (r*r)) * (2 * (pi *r));
+}
+	
